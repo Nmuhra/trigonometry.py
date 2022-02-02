688917734593780319937356b869fe869bf2f21d
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-02-02 20:58:16 +0200

    Add files via upload

---
+++ trig.py
@@ -0,0 +1,42 @@
+# let's do trigonometry
+# A^2 + B^2 = C^2
+
+from cmath import sqrt
+
+
+name= input("First give us your name please?\n")
+
+print("Hi " +name + " let's do some trig together X>")
+
+sides = "hypotenuse, opposite, adjacent"
+
+unites=  "CM, M, Feet, KM"
+
+print("what is your unit of measurment?\n "+ unites)
+
+unit = input()
+
+print("which side are you missing?\n "+ sides)
+
+side = input()
+
+if side == "hypotenuse":
+    opposite=  int(input("opposite?\n"))
+    adjacent=  int(input("adjacent?\n"))
+    hypotenuse= sqrt(opposite**2 + adjacent**2)
+    print(f"the Hypotenuse will be equal to {hypotenuse}")
+    print(unit)
+
+if side == "opposite":
+    hypotenuse=  int(input("hypotenuse?\n"))
+    adjacent=  int(input("adjacent?\n"))
+    opposite= sqrt(hypotenuse**2 - adjacent**2)
+    print(f"the opposite will be equal to {opposite}")
+    print(unit)
+
+if side == "adjacent":
+    hypotenuse=  int(input("hypotenuse?\n"))
+    opposite=  int(input("opposite?\n"))
+    adjacent= sqrt(hypotenuse**2 - opposite**2)
+    print(f"the adjacent will be equal to {adjacent}")
+    print(unit)
