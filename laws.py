2b2b994f11e0fa3c225f7e9ce057f9d5350d616b
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-03-02 07:20:12 +0200

    Create laws.py
    law of cos/sin

---
+++ laws.py
@@ -0,0 +1,12 @@
+import math
+situation = input("which situation do you have (sss, sas):\n")
+
+if situation == "sas":
+  a = input("side 1:\n")
+  A = input("angle:\n")
+  b = input("side 2:\n")
+
+if situation == "sss":
+  a = input("side 1:\n")
+  b = input("side 2\n")
+  c = input("side 3:\n")
