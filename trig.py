6c9e138ae0b9ec1022822ae071c3e65cc26cc057
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-04-26 22:57:48 +0200

    Update trig.py

--- trig.py
+++ trig.py
@@ -1,25 +1,11 @@
-# let's do trigonometry
-# A^2 + B^2 = C^2
-
 from cmath import sqrt
 
-
-name= input("First give us your name please?\n")
-
-print("Hi " +name + " let's do some trig together X>")
-
 sides = "hypotenuse, opposite, adjacent"
-
 unites=  "CM, M, Feet, KM"
-
 print("what is your unit of measurment?\n "+ unites)
-
 unit = input()
-
 print("which side are you missing?\n "+ sides)
-
 side = input()
-
 if side == "hypotenuse":
     opposite=  int(input("opposite?\n"))
     adjacent=  int(input("adjacent?\n"))
